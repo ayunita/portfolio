5ac5feaaafc1cba526cb8efe8105b062d698562c
Yunita <ayunita@Motato.local>
2020-05-18 12:07:49 -0600

    Updates

---
+++ precache-manifest.7178d3ddff47410ff81e7ac7e479f79b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f13c4a9922bf25c8082e064e7f076b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bf07f65bf0cd7d89e46",
+    "url": "/static/css/main.5d8c2a9e.chunk.css"
+  },
+  {
+    "revision": "b913b9c831b41ebb38aa",
+    "url": "/static/js/2.67995742.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.67995742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf07f65bf0cd7d89e46",
+    "url": "/static/js/main.590baf20.chunk.js"
+  },
+  {
+    "revision": "ad8caf16babc2df44569",
+    "url": "/static/js/runtime-main.e65a3091.js"
+  }
+]);
